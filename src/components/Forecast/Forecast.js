6b1f9bbc428309d24187684a678bdb463be3a3d7
Symon Hasan <isymonhs@gmail.com>
2020-07-17 15:26:28 +0600

    added pegination for hourly weather forecast

--- src/components/Forecast/Forecast.js
+++ src/components/Forecast/Forecast.js
@@ -3,7 +3,10 @@ import "./Forecast.css";
 import { connect } from "react-redux";
 import { convertTempToCelcius, convertUnixTime } from "../../utils/utils";
 
-const renderWeatherData = (data) => {
+const renderWeatherData = (data , page ) => {
+  const low = page * 6;
+  const up = low + 6;
+
   return data.map((el, index) => {
     const { humidity, pressure, temp } = el;
     const wind = el.wind_speed;
@@ -12,32 +15,51 @@ const renderWeatherData = (data) => {
     const feelsLike = convertTempToCelcius(el.feels_like);
     const celTemp = convertTempToCelcius(temp);
     const time = convertUnixTime(el.dt);
-    return (
-      <div className="hf-info hf-info-data">
-        <p>{time}</p>
-        <p
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <span>
-            <img src={iconUrl} alt="" height="30px" width="30px" />
-          </span>
-          {weather}
-        </p>
-        <p>{celTemp}&deg;</p>
-        <p>{feelsLike}&deg;</p>
-        <p>{humidity}%</p>
-        <p>{pressure}hPa</p>
-        <p>{wind}M/S</p>
-      </div>
-    );
+    if (index >= low && index < up) {
+      return (
+        <div className="hf-info hf-info-data" key={index}>
+          <p>{time}</p>
+          <p
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <span>
+              <img src={iconUrl} alt="" height="30px" width="30px" />
+            </span>
+            {weather}
+          </p>
+          <p>{celTemp}&deg;</p>
+          <p>{feelsLike}&deg;</p>
+          <p>{humidity}%</p>
+          <p>{pressure}hPa</p>
+          <p>{wind}M/S</p>
+        </div>
+      );
+    }else{
+      return null;
+    }
   });
 };
 
-const renderHourlyForecast = (hourlyWeatherData) => {
+const renderPeginate = ( incrementPage , decrementPage ) => {
+  return (
+    <div className="peg-div">
+      <i
+        className="fas fa-chevron-left"
+        onClick={decrementPage}
+      ></i>
+      <i
+        className="fas fa-chevron-right"
+        onClick={incrementPage}
+      ></i>
+    </div>
+  );
+};
+
+const renderHourlyForecast = (hourlyWeatherData , page , incrementPage , decrementPage ) => {
   return (
     <div className="hf-div">
       <div className="hf-info hf-info-header">
@@ -49,7 +71,8 @@ const renderHourlyForecast = (hourlyWeatherData) => {
         <p>Pressure</p>
         <p>Wind</p>
       </div>
-      {hourlyWeatherData ? renderWeatherData(hourlyWeatherData.hourly) : null}
+      {hourlyWeatherData ? renderWeatherData(hourlyWeatherData.hourly , page ) : null}
+      {hourlyWeatherData ? renderPeginate( incrementPage , decrementPage ) : null}
     </div>
   );
 };
@@ -58,6 +81,16 @@ const renderWeeklyForecast = () => {};
 
 const Forecast = (props) => {
   const [mode, setMode] = useState("Hourly");
+  const [ page , setPage ] = useState(0);
+
+  const incrementPage = () => {
+    if( page < 7 )
+      setPage(  page + 1 );
+  }
+  const decrementPage = () => {
+    if( page > 1 )
+      setPage( page - 1 );
+  }
 
   if (props.hourlyWeatherData === undefined) {
     const lat = props.latitude;
@@ -100,7 +133,7 @@ const Forecast = (props) => {
         )}
       </div>
       {mode === "Hourly"
-        ? renderHourlyForecast(props.hourlyWeatherData)
+        ? renderHourlyForecast(props.hourlyWeatherData , page , incrementPage, decrementPage )
         : renderWeeklyForecast()}
     </div>
   );
