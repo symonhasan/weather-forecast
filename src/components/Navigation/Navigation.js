386d1481fc377b45bfe5717509aee51764fd5bdb
Symon Hasan <isymonhs@gmail.com>
2020-07-18 01:01:48 +0600

    search onchange and on enter key press  handler added

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navigation.css";
 
 const Navigation = (props) => {
+  const [searchData, setSearchData] = useState("");
+
+  const searchOnChangeHandler = (e) => {
+    setSearchData(e.target.value);
+  };
+  const searchOnEnterKeyPressHandler = (e) => {
+    
+  };
+
   return (
     <div className="top-nav">
       <p className="top-nav-title">
         <span className="fas fa-bars"></span> Weather Forecast
       </p>
-      <input className="top-nav-search" type="text" placeholder="Search Location , City..." />
+      <input
+        className="top-nav-search"
+        type="text"
+        placeholder="Search Location , City..."
+        value={searchData}
+        onChange={searchOnChangeHandler}
+        onKeyPress={(e) =>
+          e.key === "Enter" ? searchOnEnterKeyPressHandler() : null
+        }
+      />
     </div>
   );
 };
