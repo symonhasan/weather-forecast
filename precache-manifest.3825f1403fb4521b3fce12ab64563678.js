ad9d4adcdd3577c15102626b09af57052e45329b
Symon Hasan <isymonhs@gmail.com>
2020-07-18 21:20:55 +0600

    Updates

---
+++ precache-manifest.3825f1403fb4521b3fce12ab64563678.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "285f329390903a73a92e44734ec91f68",
+    "url": "/weather-forecast/index.html"
+  },
+  {
+    "revision": "a87f0b5e6de3641a7794",
+    "url": "/weather-forecast/static/css/main.eebf16b7.chunk.css"
+  },
+  {
+    "revision": "73a4fefef64447c3d228",
+    "url": "/weather-forecast/static/js/2.4099c6e6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather-forecast/static/js/2.4099c6e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a87f0b5e6de3641a7794",
+    "url": "/weather-forecast/static/js/main.4b207e22.chunk.js"
+  },
+  {
+    "revision": "a96cd891956dfff3f9cf",
+    "url": "/weather-forecast/static/js/runtime-main.d4e096a0.js"
+  },
+  {
+    "revision": "607e94c35b73979cb3b2dda3c1019048",
+    "url": "/weather-forecast/static/media/cloud.607e94c3.jpg"
+  },
+  {
+    "revision": "05cf48bb03662f31dcd8f5d943b77530",
+    "url": "/weather-forecast/static/media/drizzle.05cf48bb.jpg"
+  },
+  {
+    "revision": "c0bc74d0427ced701e22e408838b8c5e",
+    "url": "/weather-forecast/static/media/fog.c0bc74d0.jpg"
+  },
+  {
+    "revision": "e9124ea1bbf7f18f85b62afd9871e0ba",
+    "url": "/weather-forecast/static/media/snow.e9124ea1.jpg"
+  },
+  {
+    "revision": "f2d84ed898108cb513d02c93e7417c33",
+    "url": "/weather-forecast/static/media/sunny.f2d84ed8.jpg"
+  },
+  {
+    "revision": "00cd4c2dd7660116dbd840a3d47e47fc",
+    "url": "/weather-forecast/static/media/sunny2.00cd4c2d.jpg"
+  },
+  {
+    "revision": "1c911a1824ebe3560f6143c9099d81a2",
+    "url": "/weather-forecast/static/media/thunder.1c911a18.jpg"
+  }
+]);
