ac4740ed30b151a1f20ba86b058a726d7956ea07
Symon Hasan <isymonhs@gmail.com>
2020-07-18 21:44:36 +0600

    browser router config for gh page host

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      <BrowserRouter basename="/weather-forecast">
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/:location" component={City}/>
